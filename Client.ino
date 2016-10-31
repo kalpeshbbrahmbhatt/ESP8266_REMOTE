be8dc44cc32b5109c407481ab13fac41717fd6a7
kalpeshbbrahmbhatt <k2brahmbhatt@gmail.com>
2016-11-01 00:37:37 +0530

    Add files via upload

---
+++ Client.ino
@@ -0,0 +1,31 @@
+#include <ESP8266WiFi.h>
+
+const char* ssid = "ZONE";
+const char* pw = "1234567890";
+const char* host = "192.168.4.1";
+const int port = 4011;
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, pw);
+  Serial.println("Connecting to AP");
+  while(WiFi.status() != WL_CONNECTED){
+    delay(500);
+    Serial.print("."); 
+   }
+  Serial.println(" connected");
+}
+
+void loop() {
+  WiFiClient client;
+  Serial.println("Connecting to host");
+  if(!client.connect(host, port)){
+    Serial.println("...connection failed!");
+    Serial.println("Retrying in 5 seconds...");
+    delay(5000);
+    return;  
+  }
+  client.println("A");
+  Serial.println("...TCP message fired!");
+  delay(2000);
+}
---
+++ SERVER.ino
@@ -0,0 +1,33 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMesh.h>
+#include <WiFiClient.h> 
+#include <ESP8266WebServer.h>
+
+IPAddress MyIP(192,168,4,1);
+
+WiFiServer server(MyIP, 4011);
+
+/* Set these to your desired credentials. */
+const char *ssid = "NS_SECURE";
+const char *password = "1234567890";
+
+uint32_t chip_id;
+
+String resp;
+
+void setup() {
+  Serial.begin(9600);
+  delay(10);
+  WiFi.mode(WIFI_AP_STA);
+  WiFi.softAP(ssid, password);
+  server.begin();
+
+}
+
+void loop() {
+  WiFiClient client = server.available();
+  if (client) {
+    resp = client.readStringUntil('\n');
+    Serial.println(resp);
+  }
+}
